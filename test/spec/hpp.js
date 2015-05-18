65dc2470733b72fc9a8483c7d42ecad29b201922
analog-nico <nicolai.kamenzky@testrails.org>
2015-05-18 14:17:23 -0400

    Basic whitelist implementation

--- test/spec/hpp.js
+++ test/spec/hpp.js
@@ -10,6 +10,69 @@ var rp = require('request-promise').defaults({
 
 describe('HPP', function () {
 
+    describe('should validate the options', function () {
+
+        before(function (done) {
+            echoServer.start({}, done);
+        });
+
+        after(function () {
+            echoServer.stop();
+        });
+
+        it('by turning off the whitelist for invalid type', function (done) {
+
+            echoServer.start({ hpp: { whitelist: { length: 1, get 0() { throw new Error('Whitelist not deactivated!'); } } } }, function () {
+
+                rp(echoServer.url + '/search?title=PhD&firstname=John&firstname=Alice&age=40')
+                    .then(function (data) {
+                        expect(data).to.eql({
+                            query: {
+                                title: 'PhD',
+                                firstname: 'John',
+                                age: '40'
+                            },
+                            queryPolluted: {
+                                firstname: [ 'John', 'Alice' ]
+                            },
+                            body: {}
+                        });
+                        done();
+                    })
+                    .catch(done);
+
+            });
+
+        });
+
+        it('by removing non-string elements in the whitelist array', function (done) {
+
+            echoServer.start({ hpp: { whitelist: [ 'firstname', 0, 'age' ] } }, function () {
+
+                rp(echoServer.url + '/search?title=PhD&firstname=John&firstname=Alice&age=40&age=41&0=element1&0=element2')
+                    .then(function (data) {
+                        expect(data).to.eql({
+                            query: {
+                                title: 'PhD',
+                                firstname: [ 'John', 'Alice' ],
+                                age: [ '40', '41' ],
+                                0: 'element1'
+                            },
+                            queryPolluted: {
+                                0: [ 'element1', 'element2' ]
+                            },
+                            body: {}
+                        });
+                        done();
+                    })
+                    .catch(done);
+
+            });
+
+        });
+
+    });
+
     describe('should check req.query', function () {
 
         before(function (done) {
@@ -117,6 +180,56 @@ describe('HPP', function () {
                                 title: 'PhD',
                                 firstname: [ 'John', 'Alice' ],
                                 age: '40'
+                            },
+                            body: {}
+                        });
+                        done();
+                    })
+                    .catch(done);
+
+            });
+
+        });
+
+        it('with a whitelist containing one parameter', function (done) {
+
+            echoServer.start({ hpp: { whitelist: 'firstname' } }, function () {
+
+                rp(echoServer.url + '/search?title=PhD&firstname=John&firstname=Alice&age=40&age=41')
+                    .then(function (data) {
+                        expect(data).to.eql({
+                            query: {
+                                title: 'PhD',
+                                firstname: [ 'John', 'Alice' ],
+                                age: '40'
+                            },
+                            queryPolluted: {
+                                age: [ '40', '41' ]
+                            },
+                            body: {}
+                        });
+                        done();
+                    })
+                    .catch(done);
+
+            });
+
+        });
+
+        it('with a whitelist containing two parameters', function (done) {
+
+            echoServer.start({ hpp: { whitelist: [ 'firstname', 'title' ] } }, function () {
+
+                rp(echoServer.url + '/search?title=PhD&firstname=John&firstname=Alice&age=40&age=41&title=MSC')
+                    .then(function (data) {
+                        expect(data).to.eql({
+                            query: {
+                                title: [ 'PhD', 'MSC' ],
+                                firstname: [ 'John', 'Alice' ],
+                                age: '40'
+                            },
+                            queryPolluted: {
+                                age: [ '40', '41' ]
                             },
                             body: {}
                         });
@@ -337,6 +450,70 @@ describe('HPP', function () {
                                 title: 'PhD',
                                 firstname: [ 'John', 'Alice' ],
                                 age: 40
+                            }
+                        });
+                        done();
+                    })
+                    .catch(done);
+
+            });
+
+        });
+
+        it('with a whitelist containing one parameter', function (done) {
+
+            echoServer.start({ hpp: { whitelist: 'firstname' } }, function () {
+
+                rp.post({
+                        uri: echoServer.url + '/search',
+                        body: 'title=PhD&firstname=John&firstname=Alice&age=40&age=41',
+                        headers: {
+                            'content-type': 'application/x-www-form-urlencoded'
+                        }
+                    })
+                    .then(function (data) {
+                        expect(data).to.eql({
+                            query: {},
+                            queryPolluted: {},
+                            body: {
+                                title: 'PhD',
+                                firstname: [ 'John', 'Alice' ],
+                                age: '40'
+                            },
+                            bodyPolluted: {
+                                age: [ '40', '41' ]
+                            }
+                        });
+                        done();
+                    })
+                    .catch(done);
+
+            });
+
+        });
+
+        it('with a whitelist containing two parameters', function (done) {
+
+            echoServer.start({ hpp: { whitelist: [ 'firstname', 'title' ] } }, function () {
+
+                rp.post({
+                        uri: echoServer.url + '/search',
+                        body: 'title=PhD&firstname=John&firstname=Alice&age=40&age=41&title=MSC',
+                        headers: {
+                            'content-type': 'application/x-www-form-urlencoded'
+                        }
+                    })
+                    .then(function (data) {
+                        expect(data).to.eql({
+                            query: {},
+                            queryPolluted: {},
+                            body: {
+                                title: [ 'PhD', 'MSC' ],
+                                firstname: [ 'John', 'Alice' ],
+                                age: '40'
+                            },
+                            bodyPolluted: {
+                                age: [ '40', '41' ]
                             }
                         });
                         done();
