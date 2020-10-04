57c72f71020c7dbe7060eb396f97c158c1ebf84e
Kevin Chwalek <kevin.chwalek@gmx.de>
2020-10-04 15:37:13 +0200

    Replace gulp with plain npm scripts

--- test/fixtures/echoServer.js
+++ test/fixtures/echoServer.js
@@ -1,61 +1,56 @@
-'use strict';
+'use strict'
 
-var express = require('express');
-var http = require('http');
-var bodyParser = require('body-parser');
-var hpp = require('../../lib/index.js');
-var _ = require('lodash');
+var express = require('express')
+var http = require('http')
+var hpp = require('../../lib/index.js')
+var _ = require('lodash')
 
-var server = null;
+var server = null
 
 module.exports = {
-    start: function (options, done) {
+  start: function (options, done) {
+    options = options || {}
 
-        options = options || {};
+    this.stop()
 
-        this.stop();
+    var app = express()
+    server = http.createServer(app)
 
-        var app = express();
-        server = http.createServer(app);
+    if (options.bodyParser !== false) {
+      app.use(express.json())
+      app.use(express.urlencoded({ extended: true }))
+    }
 
-        if (options.bodyParser !== false) {
-            app.use(bodyParser.json());
-            app.use(bodyParser.urlencoded({extended: true}));
-        }
-
-        if (_.isArray(options.hpp)) {
-            _.forEach(options.hpp, function (settings) {
-                if (settings.path) {
-                    app.use(settings.path, hpp(settings.options));
-                } else {
-                    app.use(hpp(settings.options));
-                }
-            });
+    if (_.isArray(options.hpp)) {
+      _.forEach(options.hpp, function (settings) {
+        if (settings.path) {
+          app.use(settings.path, hpp(settings.options))
         } else {
-            app.use(hpp(options.hpp));
+          app.use(hpp(settings.options))
         }
+      })
+    } else {
+      app.use(hpp(options.hpp))
+    }
 
-        app.use(function (req, res, next) {
-            res.json({
-                query: req.query,
-                queryPolluted: req.queryPolluted,
-                body: req.body,
-                bodyPolluted: req.bodyPolluted
-            });
-        });
+    app.use(function (req, res, next) {
+      res.json({
+        query: req.query,
+        queryPolluted: req.queryPolluted,
+        body: req.body,
+        bodyPolluted: req.bodyPolluted
+      })
+    })
 
-        server.listen(4000, function () {
-            done();
-        });
-
-    },
-    stop: function () {
-
-        if (server) {
-            server.close();
-            server = null;
-        }
-
-    },
-    url: 'http://localhost:4000'
-};
+    server.listen(4000, function () {
+      done()
+    })
+  },
+  stop: function () {
+    if (server) {
+      server.close()
+      server = null
+    }
+  },
+  url: 'http://localhost:4000'
+}
